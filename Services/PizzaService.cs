8b699f5ee842d1aeef1875b1db16008fc7118101
ariel <santa.cruz.unla@gmail.com>
2023-05-24 00:41:59 -0300

    clase estatica PizzaService

---
+++ Services/PizzaService.cs
@@ -0,0 +1,38 @@
+using ContozoPizza.Models;
+
+namespace ContozoPizza.Models;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas {get;}
+    static int nextId = 3;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza{Id= 1, Name="pizzilla",IsGlutenFree = false},
+            new Pizza{Id= 2, Name="pizzanchez",IsGlutenFree = false}
+        };
+    }
+    public static List<Pizza> getAll()=>Pizzas;
+    public static Pizza? Get (int id) => Pizzas.FirstOrDefault(p=>p.Id==id);
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if(pizza is null)
+        return;
+
+        Pizzas.Remove(pizza);
+    }
+    public static void Update (Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p=>p.Id == pizza.Id);
+        if(index==-1) return;
+        Pizzas[index]=pizza;
+    }
+}
